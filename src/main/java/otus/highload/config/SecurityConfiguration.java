47a6ab88d949bbb9ddcfe1061998b968a9a60fe2
dmitrybogorad <dmbogorad1@sberbank.ru>
2022-04-08 23:30:23 +0600

    переделка страницы логина

--- src/main/java/otus/highload/config/SecurityConfiguration.java
+++ src/main/java/otus/highload/config/SecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -37,13 +38,20 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/", "/login*", "/logout", "/register", "/h2-console/**", "/webjars/**").permitAll()
+                .antMatchers("/", "/login*", "/logout", "/register", "/h2-console/**", "/webjars/**", "/css/**").permitAll()
                 .antMatchers("/user*").hasRole("USER")
                 .anyRequest().authenticated()
-        .and().formLogin().defaultSuccessUrl("/userList")
+        .and().formLogin().loginPage("/login").defaultSuccessUrl("/userList")
         .and().logout().logoutSuccessUrl("/login");
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().sameOrigin();
     }
+
+//    @Override
+//    public void configure(WebSecurity web) {
+//        web
+//                .ignoring()
+//                .antMatchers("/resources/**", "/static/**", "/static/css/**", "/js/**", "/images/**","/vendor/**","/fonts/**");
+//    }
 
 }
